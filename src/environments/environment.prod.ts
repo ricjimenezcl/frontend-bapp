3209d799bc18083096d4faefd10a1145df621c11
Ricardo Jimenez <ricjimenezcl@gmail.com>
2026-03-23 03:55:48 -0300

    fix despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@
 // NO commitear este archivo con valores reales — usa .env.example como referencia
 export const environment = {
   production: true,
-  apiUrl: '${API_URL}',
+  apiUrl: 'https://backend-bapp.onrender.com/api/v1',
   appName: 'front-bapp',
   version: '1.0.0',
-  geoapifyApiKey: '${GEOAPIFY_API_KEY}',
-  googleClientId: '${GOOGLE_CLIENT_ID}',
-  facebookAppId: '${FACEBOOK_APP_ID}',
+  geoapifyApiKey: 'bdd29470271042c4b0751a91c0d4e9f8',
+  googleClientId: '181446851130-fc2b6ppn6qpgume9guj49kieu6ac7fet.apps.googleusercontent.com',
+  facebookAppId: '864227603333716',
   mapDefaultStyle: 'liberty' as 'liberty' | 'positron' | 'dark-matter',
 };
